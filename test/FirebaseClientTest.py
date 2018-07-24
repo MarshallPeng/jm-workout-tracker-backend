ed58c430cbb642f5c8337db3d83decf433fcbc97
Jake Schaum <schaum@wisc.edu>
2018-07-23 20:25:35 -0500

    replacing with set_user test that would be easier

--- test/FirebaseClientTest.py
+++ test/FirebaseClientTest.py
@@ -9,14 +9,15 @@ class FirebaseClientTest(unittest.TestCase):
     exercise = None
     workout = None
     user = None
+    firebase_client_test = None
     def set_up(self):
         self.exercise = Exercise("Barbell curl", 135, 3, 12, 500, 11)
         self.workout = Workout("the day!!", 0, 0, 0, 0)
         self.user = User("Jake")
         self.firebase_client_test = FirebaseClient.__init__()
-    def test_add_user(self):
+    def test_set_user(self):
         self.setUp()
-        FirebaseClient.add_user(self.firebase_client_test, self.user)
+        FirebaseClient.set_user(self.firebase_client_test, self.user)
         assert(FirebaseClient.get_usertemp0(self.firebase_client_test, self.user) is not None)
         assert(FirebaseClient.get_usertemp1(self.firebase_client_test, self.user) is not None)
 
