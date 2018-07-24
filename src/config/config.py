b5f6edac1ac6762b2115be80018c0e8e26cb65a1
Jake Schaum <schaum@wisc.edu>
2018-07-23 20:56:08 -0500

    for path

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,7 @@
+from pathlib import Path
 class FirebaseConfig:
 
     DATABASE_URL = 'https://jm-workout-tracker.firebaseio.com/'
-    SDK_PATH = '../config/jm-workout-tracker-firebase-adminsdk.json'         # file path relative to FirebaseClient
-
+    SDK_PATH = Path("../src/config/").resolve()
+    SDK_PATH = str(SDK_PATH.absolute()) + '\\jm-workout-tracker-firebase-adminsdk.json'
 
--- test/FirebaseClientTest.py
+++ test/FirebaseClientTest.py
@@ -14,9 +14,9 @@ class FirebaseClientTest(unittest.TestCase):
         self.exercise = Exercise("Barbell curl", 135, 3, 12, 500, 11)
         self.workout = Workout("the day!!", 0, 0, 0, 0)
         self.user = User("Jake")
-        self.firebase_client_test = FirebaseClient.__init__()
+        self.firebase_client_test = FirebaseClient()
     def test_set_user(self):
-        self.setUp()
+        self.set_up()
         FirebaseClient.set_user(self.firebase_client_test, self.user)
         assert(FirebaseClient.get_usertemp0(self.firebase_client_test, self.user) is not None)
         assert(FirebaseClient.get_usertemp1(self.firebase_client_test, self.user) is not None)
